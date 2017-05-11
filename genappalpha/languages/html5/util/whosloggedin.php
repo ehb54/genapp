6b38f2bb3da6babb6723d0a015cd809587a550ca
alexey <alexey@a7cf969e-cfd6-45e9-a485-bcd90cad7761>
2017-05-11 18:52:04 +0000

    Whosloggedin: sessions from different type browsers are handled..

--- genappalpha/languages/html5/util/whosloggedin.php
+++ genappalpha/languages/html5/util/whosloggedin.php
@@ -4,44 +4,78 @@
 $app = json_decode( file_get_contents( "__appconfig__" ) );
 $path = $app->phpsessionpath;
 
-//$path = "/var/lib/php5/";
-
-$latest_ctime = 0;
-$latest_filename = '';    
-
-$d = dir($path);
-
-while (false !== ($entry = $d->read())) {
-  $filepath = "{$path}/{$entry}";
-    if (is_file($filepath) && filectime($filepath) > $latest_ctime) {
-    $latest_ctime = filectime($filepath);
-    $latest_filename = $entry;
-  }
-}
+//$path = "/var/lib/php5";
 
 
-if (0 === strpos($latest_filename, 'sess')) 
+// IF latest session file needed ... //////////////////////////////////////
+//$latest_ctime = 0;
+//$latest_filename = '';    
+//$d = dir($path);
+//while (false !== ($entry = $d->read())) {
+//  $filepath = "{$path}/{$entry}";
+//    if (is_file($filepath) && filectime($filepath) > $latest_ctime) {
+//    $latest_ctime = filectime($filepath);
+//    $latest_filename = $entry;
+//  }
+//}
+//if (0 === strpos($latest_filename, 'sess')) 
+//{
+//  $latest_filename_array = explode("_", $latest_filename);
+//  $sessid = end($latest_filename_array);
+//
+//  session_id($sessid);
+//}
+//session_start(); 
+//echo "List of currently logged in users: \n";
+//echo "--------------------------------\n";
+//echo "User" . "\t" . "Application" . "\n";
+//echo "--------------------------------\n";
+//
+//foreach ($_SESSION as $key=>$val){
+//if (isset($val["logon"]) && isset($val["app"]))
+//  {
+//     echo $val["logon"] . "\t" . $val["app"] . "\n";
+//  }
+//}
+/////////////////////////////////////////////////////////////////////////
+
+$path_sess = $path . "/sess_*";
+$files = glob($path_sess);
+//var_dump($files);
+
+$users = array();
+$apps  = array();
+
+foreach($files as $file) 
 {
-  $latest_filename_array = explode("_", $latest_filename);
-  $sessid = end($latest_filename_array);
+  $file_array = explode("_", $file);
+  $sessid = end($file_array);
+     
+  //echo $sessid . "\n"; 
 
   session_id($sessid);
-}
+  session_start();
 
-session_start(); 
+  foreach ($_SESSION as $key=>$val){
+  if (isset($val["logon"]) && isset($val["app"]))
+     {
+        $users[] = $val['logon'];
+	$apps[] = $val['app'];
+	//echo $val["logon"] . "\t" . $val["app"] . "\n";
+     }	
+  } 
+ session_commit();
+}
 
 echo "List of currently logged in users: \n";
 echo "--------------------------------\n";
 echo "User" . "\t" . "Application" . "\n";
 echo "--------------------------------\n";
 
-foreach ($_SESSION as $key=>$val){
-if (isset($val["logon"]) && isset($val["app"]))
-  {
-     echo $val["logon"] . "\t" . $val["app"] . "\n";
-  }
+foreach ($users as $index => $value){
+  echo $users[$index] . "\t" . $apps[$index] . "\n";
 }
-
+//var_dump($users);
 //var_dump($_SESSION);
 
 
