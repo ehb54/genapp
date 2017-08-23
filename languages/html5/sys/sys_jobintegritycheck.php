d8a1bebb37923721b8b78cb18136ef9c5cfc19ea
ehb <ehb54@users.noreply.github.com>
2017-08-23 22:00:06 +0000

    Web based job integrity with working repair

--- languages/html5/sys/sys_jobintegritycheck.php
+++ languages/html5/sys/sys_jobintegritycheck.php
@@ -24,7 +24,7 @@ if ( !isset( $_REQUEST[ '_logon' ] ) ) {
     exit();
 }
 
-$appconfig = json_decode( file_get_contents( "__appconfig__" ) );
+$appconfig = json_decode( file_get_contents( "/home/ehb/demo/appconfig.json" ) );
 
 if ( !isset( $appconfig->restricted ) ) {
     $results[ "error" ] = "appconfig.json no restrictions defined";
@@ -73,8 +73,163 @@ $allresources = array_keys( (array) $appconfig->resources );
 
 $results = [];
 
-$results[ 'jobintegrityreport' ] = "jobintegrityreport";
+db_connect( true );
 
+function integrity() {
+    global $use_db;
+    global $db_errors;
+    global $todo;
+    global $cmdlineeq;
+    global $runningremove;
+    global $appsbyid;
+    global $pidkill;
+    $retval = "";
+
+# -------------- 1st get all apps  --------------
+
+    $apps = [];
+    $docs = $use_db->global->apps->find();
+    foreach ( $docs as $this_doc ) {
+        $apps[] =  $this_doc[ "_id" ];
+    }
+
+    foreach ( $apps as $v ) {
+        $retval .= "apps $v\n";
+    }
+
+# -------------- get all php jobrun ids --------------
+
+    $cmd = "env COLUMNS=1000 ps -ef | grep jobrun.php | grep -v grep | awk '{ print \$2 \":\" \$10 \":\" \$11 }'";
+
+    $results = `$cmd`;
+    $phpids = preg_split( "/\n/", $results, -1, PREG_SPLIT_NO_EMPTY );
+
+    $phpidsa = [];
+    $allidsa = [];
+
+    foreach ( $phpids as $v ) {
+        $results = preg_match( '/^([^:]*):([^:]*):([^:]*)$/', $v, $matches );
+        $pid = $matches[ 1 ];
+        $user = $matches[ 2 ];
+        $jid = $matches[ 3 ];
+        
+#        if ( $jid == $_REQUEST[ "_uuid" ] ) {
+#            continue;
+#        }
+        $retval .= "phpid [$v]\n";
+        $pids[ $jid ] = $pid;
+        $users[ $jid ] = $user;
+        $phpidsa[ $jid ] = 1;
+        $allidsa[ $jid ] = 1;
+    }
+
+# -------------- get all running job ids --------------
+
+    $runningidsa = [];
+
+    foreach ( $apps as $v ) {
+        $retval .= "check running apps $v\n";
+
+        $docs = $use_db->{$v}->running->find();
+
+        $runningids = [];
+        foreach ( $docs as $this_doc ) {
+            $runningids[] = $this_doc[ "_id" ];
+        }
+
+        $retval .= "------------------------------------------------------------\n";
+
+#        if ( isset( $runningids[ $_REQUEST[ "_uuid" ] ] ) ) {
+#            unset( $runningids[ $_REQUEST[ "_uuid" ] ] );
+#        }
+
+        foreach ( $runningids as $v2 ) {
+            $retval .= "runningid $v2\n";
+            $runningidsa[ $v2 ] = 1;
+            $allidsa[ $v2 ] = 1;
+            $appsbyid[ $v2 ] = $v;
+        }
+    }
+
+    $todo = [];
+
+    $runningremove = [];
+    $cmdlineeq = [];
+    $pidkill = [];
+
+    foreach ( $allidsa as $k => $v ) {
+        $disposition = "";
+        if ( isset( $phpidsa[ $k ] ) && isset( $runningidsa[ $k ] ) ) {
+            $disposition = "ok $users[$k]";
+        } else {
+            if ( isset( $phpidsa[ $k ] ) ) {
+                $disposition = "kill php";
+                if ( !isset( $todo[ $users[ $k ] ] ) ) {
+                    $todo[ $users[ $k ] ] = "";
+                }
+                $todo[ $users[ $k ] ] .= "sudo kill $pids[$k]\n";
+                $pidkill[] = $k;
+            } else {
+                $disposition = "remove running";
+                if ( !isset( $users[ $k ] ) ) {
+                    $users[ $k ] = "unknown";
+                }
+                if ( !isset( $todo[ $users[ $k ] ] ) ) {
+                    $todo[ $users[ $k ] ] = "";
+                }
+                $cmdlineeq[ $k ] = "mongo $appsbyid[$k] --eval 'db.running.remove({_id:\"$k\"})'\n";
+                $runningremove[] = $k;
+            }
+        }
+
+        $retval .= "$k $disposition\n";
+    }
+
+    foreach ( $todo as $k => $v ) {
+        $retval .= "----------------------------------------\n";
+        $retval .= "$k\n";
+        $retval .= "----------------------------------------\n";
+        $retval .= $v;
+    }
+    return $retval;
+}
+
+
+$results[ '_textarea' ] = integrity();
+if ( !count( $pidkill ) && !count( $runningremove ) ) {
+    $results[ '_textarea' ] .= "========================================\n";
+    $results[ '_textarea' ] .= "All ok\n";
+    $results[ '_textarea' ] .= "========================================\n";
+    $results[ 'jobintegrityreport' ] = "";
+} else {
+    if ( $_REQUEST[ "fixerrors" ] ) {
+        $results[ '_textarea' ] .= "========================================\n";
+        foreach ( $pidkill as $v ) {
+            $results[ '_textarea' ] .= "posix_kill( $v )";
+            posix_kill( $v, SIGTERM );
+        }
+        foreach ( $runningremove as $v ) {
+            $results[ '_textarea' ] .= "mongo remove $appsbyid[$v] $v\n";
+            $use_db->{$appsbyid[$v]}->running->remove( [ "_id" => $v ] );
+        }
+            
+        $results[ '_textarea' ] .= "========================================\n";
+        $results[ 'jobintegrityreport' ] = "Errors present, fixed.";
+    } else {
+        $results[ 'jobintegrityreport' ] = "Errors present.\n\n";
+        $results[ '_textarea' ] .= "========================================\n";
+        $results[ '_textarea' ] .= "Command line fixes posssible:\n";
+        $results[ '_textarea' ] .= "========================================\n";
+
+        foreach ( $todo as $k => $v ) {
+            $results[ '_textarea' ] .= "$v";
+        }
+        foreach ( $runningremove as $v ) {
+            $results[ '_textarea' ] .= $cmdlineeq[ $v ];
+        }
+    }
+}
+    
 echo json_encode( $results );
 
 ?>
