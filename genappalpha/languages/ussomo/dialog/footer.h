eb02aa200389e2cfe4810965aa51db3474723063
ehb <ehb@a7cf969e-cfd6-45e9-a485-bcd90cad7761>
2017-12-19 18:27:27 +0000

    initial commit ussomo language for building input dialogs compatible with ussomo

---
+++ genappalpha/languages/ussomo/dialog/footer.h
@@ -0,0 +1,7 @@
+
+   protected slots:
+
+      void                                    closeEvent( QCloseEvent * );
+};
+
+#endif
---
+++ genappalpha/languages/ussomo/dialog/middle.h
@@ -0,0 +1,5 @@
+   private slots:
+
+      void                                    help();
+      void                                    cancel();
+
