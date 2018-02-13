b509d00ade2f325200e323d0b1e1041a61f8aea7
ehb <ehb54@users.noreply.github.com>
2018-02-13 18:00:27 +0000

    add register support to language json add register keyword to html5 language better message for qt4's make

--- languages/qt4/make.sh
+++ languages/qt4/make.sh
@@ -1,4 +1,5 @@
 cd __application__
+command -v qmake >/dev/null 2>&1 || { echo >&2 "Install qt4 make sure qmake is in your path to compile programs in the target language 'qt4'."; exit 1; }
 qmake -project
 qmake
 make
