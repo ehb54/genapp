9f225f8e21d812f51d8ed5609e7f69e48cddff28
ehb <ehb54@users.noreply.github.com>
2018-10-01 17:04:26 +0000

    hopefully a fix for stale locks

--- languages/html5/util/msg-udpserver.php
+++ languages/html5/util/msg-udpserver.php
@@ -10,6 +10,7 @@ if ( isset( $json->lockdir ) ) {
 
 // check if already running and register pid
 define('LOCK_FILE', "$lockdir/msg-udp-" . $json->messaging->zmqport . ".lock");
+define('EXPECTED_CMDLINE', "phpmsg-udpserver.php" );
 
 if ( !tryLock() ) {
    die( "Already running.\n" );
@@ -41,15 +42,52 @@ function tryLock() {
     # Else, create lock file and return FALSE.
 
     if (@symlink("/proc/" . getmypid(), LOCK_FILE) !== FALSE) # the @ in front of 'symlink' is to suppress the NOTICE you get if the LOCK_FILE exists
+    {   
         return true;
+    }
 
     # link already exists
     # check if it's stale
+    $isstale = false;
+
+    if ( is_link(LOCK_FILE) ) {
+        echo "is_link(" . LOCK_FILE . ") true\n";
+        if ( ( $link = readlink( LOCK_FILE ) ) === FALSE ) {
+            $isstale = true;
+            echo "is stale 1\n";
+        }
+    } else {
+        $isstale = true;
+        echo "is stale 2\n";
+    }
+
+    if ( !$isstale && is_dir( $link ) ) {
+        # make sure the cmdline exists & matches expected
+        $cmdline_file = $link . "/cmdline";
+        echo "cmdline_file = $cmdline_file\n";
+        if ( ($cmdline = file_get_contents( $cmdline_file )) === FALSE ) {
+            echo "could not get contents of $cmdline_file\n";
+            $isstale = true;
+            echo "is stale 3\n";
+        } else {
+            # remove nulls
+            $cmdline = str_replace("\0", "", $cmdline);
+            if ( $cmdline != EXPECTED_CMDLINE ) {
+                echo "unexpected contents of $cmdline_file\n";
+                $isstale = true;
+                echo "is stale 4 \n";
+            }
+        }
+    }            
+        
     if (is_link(LOCK_FILE) && !is_dir(LOCK_FILE)) {
+        $isstale = true;
+    }
+
+    if ( $isstale ) {
         unlink(LOCK_FILE);
         # try to lock again
         return tryLock();
     }
-
     return false;
 }
--- languages/html5/util/msg-wsserver.php
+++ languages/html5/util/msg-wsserver.php
@@ -20,22 +20,60 @@ if ( isset( $json->lockdir ) ) {
 
 // check if already running and register pid
 define('LOCK_FILE', "$lockdir/msg-ws-" . $json->messaging->zmqport . ".lock");
+define('EXPECTED_CMDLINE', "phpmsg-wsserver.php" );
 
 function tryLock() {
     # If lock file exists, check if stale.  If exists and is not stale, return TRUE
     # Else, create lock file and return FALSE.
 
     if (@symlink("/proc/" . getmypid(), LOCK_FILE) !== FALSE) # the @ in front of 'symlink' is to suppress the NOTICE you get if the LOCK_FILE exists
+    {   
         return true;
+    }
 
     # link already exists
     # check if it's stale
+    $isstale = false;
+
+    if ( is_link(LOCK_FILE) ) {
+        echo "is_link(" . LOCK_FILE . ") true\n";
+        if ( ( $link = readlink( LOCK_FILE ) ) === FALSE ) {
+            $isstale = true;
+            echo "is stale 1\n";
+        }
+    } else {
+        $isstale = true;
+        echo "is stale 2\n";
+    }
+
+    if ( !$isstale && is_dir( $link ) ) {
+        # make sure the cmdline exists & matches expected
+        $cmdline_file = $link . "/cmdline";
+        echo "cmdline_file = $cmdline_file\n";
+        if ( ($cmdline = file_get_contents( $cmdline_file )) === FALSE ) {
+            echo "could not get contents of $cmdline_file\n";
+            $isstale = true;
+            echo "is stale 3\n";
+        } else {
+            # remove nulls
+            $cmdline = str_replace("\0", "", $cmdline);
+            if ( $cmdline != EXPECTED_CMDLINE ) {
+                echo "unexpected contents of $cmdline_file\n";
+                $isstale = true;
+                echo "is stale 4 \n";
+            }
+        }
+    }            
+        
     if (is_link(LOCK_FILE) && !is_dir(LOCK_FILE)) {
+        $isstale = true;
+    }
+
+    if ( $isstale ) {
         unlink(LOCK_FILE);
         # try to lock again
         return tryLock();
     }
-
     return false;
 }
 
