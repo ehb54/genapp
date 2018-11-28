11ad3ef67dbc610d228306ca824a5031add8ac7f
ehb <ehb@a7cf969e-cfd6-45e9-a485-bcd90cad7761>
2018-11-28 15:50:44 +0000

    tmp updates

--- genappalpha/tmp/syncfilesdirs.php
+++ genappalpha/tmp/syncfilesdirs.php
@@ -1,9 +1,36 @@
 <?php
     ;
 
-$credfile = "/opt/genapp/docker/tutorial2/results/bin/my_seedme2_conf.txt";
-    
 $debug = 0;
+
+$notes = <<<__EOD
+usage: php $argv[0] --cred seedme2_credentials_file --fs fs_base_directory
+__EOD;
+
+$options = getopt(
+    ""
+    ,[
+     "cred:"
+     ,"fs:"
+    ]
+    );
+
+debugecho( json_encode( $options, JSON_PRETTY_PRINT ), 0 );
+
+if ( !isset( $options[ "cred" ] ) || !strlen( $options[ "cred" ] ) ) {
+    echo $notes;
+    exit( 1 );
+}
+
+if ( !isset( $options[ "fs" ] ) || !strlen( $options[ "fs" ] ) ) {
+    echo $notes;
+    exit( 1 );
+}
+
+$credfile = $options[ "cred" ];
+$fs       = $options[ "fs" ];
+
+# setup foldershare process
 
 $process = proc_open(". $credfile && foldershare --host \$host --username \$username --password \$password",
                   [
@@ -23,13 +50,7 @@ stream_set_blocking( $pipes[0], false );
 stream_set_blocking( $pipes[1], false );
 stream_set_blocking( $pipes[2], false );
 
-# setup command stack
-
-$cmds = [
-    [ "waitfor" => "foldershare> " ]
-    ,[ "runcmd" => "ls -l" ]
-    ,[ "waitfor" => "foldershare> " ]
-    ];
+# setup command stack to extract seedme2 file listing
 
 $towrite   = [];
 $waitfor   = [];
@@ -37,18 +58,17 @@ $responses = "";
 $lastcmd   = "";
 $waitone   = 0;
 $g_info    = 
-    [ 
-      "files" => []
-      ,"dirs" => []
+    [
+     "seedme2" => [
+         "files" => []
+         ,"dirs" => []
+     ]
+     ,"fs" => [
+         "files" => []
+         ,"dirs" => []
+     ]
     ];
 
-function finishup() {
-    global $g_info;
-
-    echo json_encode( $g_info, JSON_PRETTY_PRINT ) . "\n";
-
-    exit;
-}
 
 function debugecho ( $str, $level = 1 ) {
     global $debug;
@@ -57,6 +77,8 @@ function debugecho ( $str, $level = 1 ) {
     }
 }
     
+# process individual commands
+
 function nextcmd() {
     global $cmds;
     global $towrite;
@@ -109,9 +131,9 @@ function nextcmd() {
                     # we could add data to this entry, e.g. timestamp, size etc
                     preg_match( '/\s+Size:\s(\d+)\s/', $responses, $matches );
                     $size = $matches[ 1 ];
-                    preg_match( '/Modify:\s(.{23})/', $responses, $matches );
+                    preg_match( '/Modify:\s(.{18,19} \d{4})/', $responses, $matches );
                     $modify = $matches[ 1 ];
-                    preg_match( '/Create:\s(.{23})/', $responses, $matches );
+                    preg_match( '/Create:\s(.{18,19} \d{4})/', $responses, $matches );
                     $create = $matches[ 1 ];
                     $info = [
                         "size" => $size
@@ -124,12 +146,12 @@ function nextcmd() {
                         case "Directory" : {
                             array_push( $cmds, [ "runcmd" => "ls -l '$basedir'" ] );
                             array_push( $cmds, [ "waitfor" => "foldershare> " ] );
-                            array_push( $g_info[ "dirs" ], [ $basedir => $info ] );
+                            array_push( $g_info[ "seedme2" ][ "dirs" ], [ $basedir => $info ] );
                         }
                         break;
 
                         case "Regular" : {
-                            array_push( $g_info[ "files" ], [ $basedir => $info ] );
+                            array_push( $g_info[ "seedme2" ][ "files" ], [ $basedir => $info ] );
                         }
                         break;
                         
@@ -182,65 +204,163 @@ function nextcmd() {
     return 1;
 }
     
+# command loop
 
-if ( !nextcmd() ) {
-    echo "Empty command stack!\n";
+function process_cmds () {
+    global $pipes;
+    global $waitfor;
+    global $towrite;
+    global $responses;
+    global $process;    
+
+    # are we setup?
+
+    if ( !nextcmd() ) {
+        echo "Empty command stack!\n";
+        exit;
+    }
+
+    # process until cmds exhausted
+
+    do {
+        debugecho( "select loop" );
+        
+        $read = [ 
+            $pipes[ 2 ] 
+            ];
+
+        if ( count( $waitfor ) ) {
+            array_push( $read, $pipes[ 1 ] );
+        }
+
+        if ( count( $towrite ) ) {
+            $write = [
+                $pipes[ 0 ]
+                ];
+        } else {
+            $write = [];
+        }
+        $except = [];
+
+        $retval = stream_select( $read, $write, $except, 1 );
+        debugecho( "select loop return value : $retval" );
+        if ( !$retval ) {
+            $status = proc_get_status( $process );
+            if ( !$status[ 'running' ] ) {
+                echo "Error, process died\n";
+                exit;
+            }
+            continue;
+        }
+
+        # there are streams to process
+        if ( in_array( $pipes[ 1 ], $read ) ) {
+            $responses .= stream_get_contents( $pipes[ 1 ] );
+            if ( strpos( $responses, $waitfor[ 0 ] ) ) {
+                array_shift( $waitfor );
+                if ( !nextcmd() ) {
+                    return;
+                }
+            }
+        }
+        
+        if ( in_array( $pipes[ 2 ], $read ) ) {
+            echo "Error returned by process:" . stream_get_contents( $pipes[ 2 ] );
+            exit( 1 );
+        }
+
+        if ( in_array( $pipes[ 0 ], $write ) ) {
+            fwrite( $pipes[ 0 ], array_shift( $towrite ) . "\n");
+            if ( !nextcmd() ) {
+                return;
+            }
+        }
+    } while ( 1 );
+}
+
+# get local fs
+
+function scan_fs( $dir = "" ) {
+    global $g_info;
+    global $fs;
+
+    $use_dir = $fs;
+    if ( strlen( $dir ) ) {
+        $use_dir = $fs . "/" . $dir;
+    }
+
+    $sdir = scandir( $use_dir );
+    foreach ( $sdir as $key => $value ) {
+        if ( !in_array( $value, [ ".", ".." ] ) ) {
+            $name = $dir .  "/" . $value;
+            $info = [];
+
+            if ( is_dir( $use_dir . "/" . $value ) ) {
+                array_push( $g_info[ "fs" ][ "dirs" ], [ $name => $info ] );
+                scan_fs( $name );
+            } else {
+                array_push( $g_info[ "fs" ][ "files" ], [ $name => $info ] );
+            }
+        }
+    }                
+}
+
+function local_fs() {
+    debugecho( "local_fs", 0 );
+    scan_fs();
+}
+
+# work thru the stages
+# stages are
+#  1: get seedme2 fs info
+#  2: get local fs info
+#  3: sync dirs
+#  4: sync files
+
+$startatstage  = 2;
+$finishatstage = 2;
+
+function stage_loop() {
+    global $g_info;
+    global $finishatstage;
+    global $startatstage;
+    global $cmds;
+    
+    for ( $stage = $startatstage; $stage <= $finishatstage; $stage++ ) {
+
+        switch ( $stage ) {
+            case 1 : { # getseedme2 fs info
+                debugecho( "stage $stage", 0 );
+
+                $cmds = [
+                    [ "waitfor" => "foldershare> " ]
+                    ,[ "runcmd" => "ls -l" ]
+                    ,[ "waitfor" => "foldershare> " ]
+                    ];
+                process_cmds();
+            }
+            break;
+            
+            case 2 : { # get local fs info
+                debugecho( "stage $stage", 0 );
+                local_fs();
+            }
+            break;
+
+            default : { # no more stages
+                debugecho( "stage $stage", 0 );
+                debugecho( "no more stages", 0 );
+            }
+            break;
+        }
+
+    }
+
+    debugecho( "stage loop done", 0 );
+    echo json_encode( $g_info, JSON_PRETTY_PRINT ) . "\n";
+
     exit;
 }
 
-do {
-    debugecho( "select loop" );
-    
-    $read = [ 
-        $pipes[ 2 ] 
-        ];
-
-    if ( count( $waitfor ) ) {
-        array_push( $read, $pipes[ 1 ] );
-    }
-
-    if ( count( $towrite ) ) {
-        $write = [
-            $pipes[ 0 ]
-            ];
-    } else {
-        $write = [];
-    }
-    $except = [];
-
-    $retval = stream_select( $read, $write, $except, 1 );
-    debugecho( "select loop return value : $retval" );
-    if ( !$retval ) {
-        $status = proc_get_status( $process );
-        if ( !$status[ 'running' ] ) {
-            echo "Error, process died\n";
-            exit;
-        }
-        continue;
-    }
-
-    # there are streams to process
-    if ( in_array( $pipes[ 1 ], $read ) ) {
-        $responses .= stream_get_contents( $pipes[ 1 ] );
-        if ( strpos( $responses, $waitfor[ 0 ] ) ) {
-            array_shift( $waitfor );
-            if ( !nextcmd() ) {
-                finishup();
-            }
-        }
-    }
-         
-    if ( in_array( $pipes[ 2 ], $read ) ) {
-        echo "Error returned by process:" . stream_get_contents( $pipes[ 2 ] );
-        exit( 1 );
-    }
-
-    if ( in_array( $pipes[ 0 ], $write ) ) {
-        fwrite( $pipes[ 0 ], array_shift( $towrite ) . "\n");
-        if ( !nextcmd() ) {
-            finishup();
-        }
-    }
-
-} while ( 1 );            
+stage_loop();
 
