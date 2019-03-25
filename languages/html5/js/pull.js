6fd4d079a42d2569f6203ac859452b876d2354a8
ehb <ehb54@users.noreply.github.com>
2019-03-25 14:10:32 +0000

    Allow "pull" for non-login (server datetime)

--- languages/html5/js/pull.js
+++ languages/html5/js/pull.js
@@ -112,5 +112,67 @@ __~debug:pull{    console.log( "ga.pull.doPull(): ajax fail" );}
       } else {
 __~debug:pull{      console.log( "ga.pull.doPull(): doPull t has NO size" );}
       }
+   } else {
+// for no login
+      var tj = gd.data( "_pull_json" + repeater );
+      tj[ "_window" ] = window.name;
+__~debug:pull{   console.log( "ga.pull.doPull() tj:" + tj );}
+      if ( Object.size( tj ) > 1 )
+      {
+__~debug:pull{      console.log( "ga.pull.doPull(): has size" );}
+         $.getJSON( "ajax/sys_config/sys_pull.php", tj )
+         .done( function( data, status, xhr ) {
+__~debug:pull{    console.log( "ga.pull.doPull(): ajax done data:" );}
+__~debug:pull{    console.log( data );}
+            var tu = gd.data( "_pull_update" + repeater );
+            $.each(data, function(k, v) {
+__~debug:pull{    console.log( "ga.pull.doPull(): " + k + " => " + v );}
+__~debug:pull{    console.log( "ga.pull.doPull(): typeof tu[k] " + typeof( tu[ k ] ) );}
+               if ( typeof( tu[ k ] ) == "object" )
+               {
+                  $.each( tu[ k ], function( k2, v2 ) {
+__~debug:pull{    console.log( "ga.pull.doPull(): in tu[k]:" + k2 + " => " + v2 );}
+                     var t = $( k2 );
+                     switch( v2 )
+                     {
+                        case "checkbox" : 
+                         t.prop( "checked", v == "on" ); break;
+                        case "text" : 
+                         if( t.attr( "data-type" ) == "color" ) {
+                             __~debug:spectrum{console.log( "got data-color" );}
+                             ga.color.spectrum.val( k2, v );
+                         }
+                        case "email" : 
+                        case "text" : 
+                        case "integer" : 
+                        case "float" : 
+                         t.val( v ); break;
+                        case "listbox" : 
+                         t.empty();
+// setup html for results
+__~debug:pull{   console.log( "ga.pull.doPull(): listbox pull'd " + typeof( v ) ); }
+__~debug:pull{   console.log( v ); }
+                         $.each( v, function( k3, v3 ) {
+__~debug:pull{   console.log( "ga.pull.doPull(): listbox append " + v3 ); }
+                           t.append($("<option></option>").attr( "value", v3 ).text( v3 ) );
+                         });
+                         break;
+                        case "label" : 
+__~debug:pull{   console.log( "ga.pull.doPull(): label for " + k2 ); }
+                         t.html( v );
+                         break;
+                        default : 
+                         console.log( "ga.pull.doPull(): not yet" );
+                     }
+                  });
+               }
+            });
+         })
+         .fail( function( xhr, status, errorThrown ) {
+__~debug:pull{    console.log( "ga.pull.doPull(): ajax fail" );}
+         });
+      } else {
+__~debug:pull{      console.log( "ga.pull.doPull(): doPull t has NO size" );}
+      }
    }
 }
