ac83308b6d35b4fcacf9c3ecaca91f379769e72f
ehb54 <ehb54@users.noreply.github.com>
2019-04-30 10:40:46 -0500

    Docker: build needed non-interpolating heredoc

--- languages/docker/build_docker_module.pl
+++ languages/docker/build_docker_module.pl
@@ -1,11 +1,11 @@
 # --------- module __menu:modules:id__ ----------
 
-$dependencies{ "__menu:modules:id__" } = <<__EOF;
+$dependencies{ "__menu:modules:id__" } = <<'__EOF';
 __dependencies__
 __EOF
     ;
 
-$module_json{ "__menu:modules:id__" } = <<__EOF;
+$module_json{ "__menu:modules:id__" } = <<'__EOF';
 __modulejson__
 __EOF
     ;
