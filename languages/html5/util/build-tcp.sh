3722c65c4056a42949a32a20312e76b283f2aa96
ehb54 <brookes@uthscsa.edu>
2021-10-12 12:51:46 +0000

    build-tcp - avoid useless application builds

--- languages/html5/util/build-tcp.sh
+++ languages/html5/util/build-tcp.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-go get github.com/ehb54/lockfile
-go get github.com/ehb54/zmq__zmqversion__
-cd util && go build msg-tcpserver.go
+zmq="__zmqversion__"
+if (( ${#zmq}==1 ))
+then   
+    go get github.com/ehb54/lockfile
+    go get github.com/ehb54/zmq__zmqversion__
+    cd util && go build msg-tcpserver.go
+else
+    echo "no directives:zmqversion defined, will not build msg-tcpserver - only needed by genapptest application"
+fi
+
