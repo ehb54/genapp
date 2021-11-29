1e32b459928d923a84b155ed954998b50675f8ec
ehb54 <brookes@uthscsa.edu>
2021-11-29 21:58:01 +0000

    activate prebuilt scss themes

--- languages/html5/scss/scss-build.sh
+++ languages/html5/scss/scss-build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+__!bootstrap:forcemake{echo use directives:bootstrap:forcemake to make scss && exit}
 __~bootstrap:package{cd scss && cp __bootstrap:package__-package.json package.json && npm install && npm run css}
