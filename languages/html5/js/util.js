20be116f9ded19a05ddab079fbc798bd898cad17
ehb54 <brookes@uthscsa.edu>
2022-10-12 04:04:13 +0000

    util.js / fix for jqgrid reset

--- languages/html5/js/util.js
+++ languages/html5/js/util.js
@@ -26,6 +26,12 @@ ga.util.jqgrid.filter = function( jqgridid, runningid, moduleid, projectid ) {
         return;
     }
     
+    // reset grid
+    var grid = $(`#${jqgridid}`);
+    grid.jqGrid('setGridParam',{search:false});
+    var postData = grid.jqGrid('getGridParam','postData');
+    $.extend(postData,{filters:""});
+    
     rules = [];
     
     if ( projectval != all ) {
@@ -35,9 +41,11 @@ ga.util.jqgrid.filter = function( jqgridid, runningid, moduleid, projectid ) {
         rules.push( { field: moduleid, op: "cn", data: `/${moduleval}<` } );
     }
 
+    __~debug:jqgrid{console.dir(rules);}
+
     // add running rule (needs first mod to grid data
 
-    $(`#${jqgridid}`).jqGrid("setGridParam", {
+    grid.jqGrid("setGridParam", {
         postData: {
             filters: {
                 groupOp : "AND",
