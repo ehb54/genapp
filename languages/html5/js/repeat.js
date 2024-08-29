1c7b20da6a106a4d72e55144b8bc65099537349d
ehb54 <brookes@uthscsa.edu>
2024-08-29 19:00:03 +0000

    repeat.js - check object key fix

--- languages/html5/js/repeat.js
+++ languages/html5/js/repeat.js
@@ -682,7 +682,7 @@ ga.repeat.changeMany = function( mod, data ) {
     // build up list of repeaters sorted by number of -, then call ga.data.update
     
     Object.keys( data )
-        .filter( k => typeof ga.repeat.data[mod].repeater[k] === 'object' )
+        .filter( k => ga.repeat.data[mod] && typeof ga.repeat.data[mod].repeater[k] === 'object' )
         .sort( ( a, b ) => ( a.match(/-/g) || [] ).length > ( b.match(/-/g) || [] ).length )
     // ECMAScript 2015 (ES6) (doesn't seem to work in current Firefox linux, OSX, 2022-09-26)
     //        .map( k => { ga.data.update( mod, { [k] : data[k] }, true ); ga.repeat.change( mod, k ) } ); 
